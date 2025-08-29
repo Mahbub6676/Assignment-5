4d0a3985e8d276d76ecdc9c81a36ad9fe0d3aadc
Mahbub6676 <mahbub200211@gmail.com>
2025-08-29 20:30:05 +0600

    added copy btn functionality

--- script.js
+++ script.js
@@ -11,26 +11,33 @@ for (const hrt of hearts) {
 }
 
 
-//5. Call Buttons
+// Call Buttons
 let coins = 100;
 const coinDisplay = document.getElementById("coin-btn");
 const callButtons = document.querySelectorAll("#call-btn");
-// const historyList = document.getElementById("history-list");
 
 for (const btn of callButtons) {
-    btn.addEventListener("click", function() {
+    btn.addEventListener("click", function () {
         const service = btn.getAttribute("data-service");
         const number = btn.getAttribute("data-number");
-
         if (coins < 20) {
             alert("Not enough coins for a call!");
             return;
         }
-
         coins -= 20;
         coinDisplay.innerText = coins;
-
         alert(`Calling ${service} at ${number}...`);
+    });
+}
+
+
+let buttons = document.querySelectorAll(".copy-btn");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        let number = this.getAttribute("data-number");
+        navigator.clipboard.writeText(number)
+        alert("Copied: " + number);
     });
 }
 
